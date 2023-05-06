b09ed9ac736cd45a9c30e8b07f1a4adf64156a12
simozampa <simozampa@ucla.edu>
2023-05-06 14:27:27 -0700

    Add types to app

---
+++ app/api/product/new-product.tsx
@@ -0,0 +1,13 @@
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handle(req: NextApiRequest, res: NextApiResponse) {
+    const { method } = req;
+    if(method === 'POST') {
+        
+    }
+
+    if(method === 'GET') {
+
+    }
+}

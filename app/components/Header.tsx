93108023ef17a96df4abb2308d316676645eba55
simozampa <simozampa@ucla.edu>
2023-05-09 16:39:51 -0700

    Place Header and Footer in layout

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Fragment, useState } from 'react'
 import { Dialog, Popover, Tab, Transition } from '@headlessui/react'
 import { Bars3Icon, MagnifyingGlassIcon, ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/outline'
--- app/models/Product.ts
+++ app/models/Product.ts
@@ -1,9 +1,9 @@
 import { Schema, models, model } from "mongoose";
 
 const ProductSchema = new Schema({
-    title: {type: String, required: true},
+    title: { type: String, required: true },
     description: String,
-    price: {type: Number, required: true},
+    price: { type: Number, required: true },
     sizes: String,
 });
 

02d602ace90e1419adf73b82de1a07a3f24a44a3
simozampa <simozampa@ucla.edu>
2023-05-12 10:17:39 -0700

    Fix connection with Mongoose

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,21 +1,21 @@
 import { Product } from '@/app/models/Product'
 import { mongooseConnect } from '@/app/database/mongoose';
-const mongoose = require('mongoose');
 
 export async function POST(req: Request) {
-    await mongoose.connect("mongodb+srv://simozampa:<3NcsmN6RovpaLRnKpassword>@cluster0.rdkre1n.mongodb.net/?retryWrites=true&w=majority");
 
-    console.log("post works");
-    // const { title, description, price, sizes } = await req.json();
+    await mongooseConnect();
+
+    const { title, description, price, sizes } = await req.json();
+    console.log(title, description, price, sizes);
     
-    // const productDoc = Product.create({
-    //     title, 
-    //     description, 
-    //     price, 
-    //     sizes
-    // });
+    const productDoc = await Product.create({
+        title, 
+        description, 
+        price, 
+        sizes
+    });
 
-    // return new Response(JSON.stringify(productDoc));
+    return new Response(productDoc);
 }
 
 export async function GET(req: Request) {
--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: { appDir: true, serverComponentsExternalPackages: ["mongoose"] },
     webpack: (config) => {
         // this will override the experiments
         config.experiments = { ...config.experiments, topLevelAwait: true };
         // this will just update topLevelAwait property of config.experiments
         // config.experiments.topLevelAwait = true 
+        config.module.rules.push = ({
+            test: /.txt$/, use: 'raw-loader' 
+        });
+        
         return config;
       },
 }

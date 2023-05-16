e15c8b5f4298f3fa8318586e7cdf1e5d5ea0d358
simozampa <simozampa@ucla.edu>
2023-05-15 18:44:32 -0700

    File uploading frontend

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,17 +1,20 @@
-import multiparty from 'multiparty';
+import { NextRequest } from 'next/server';
 
-export async function POST(req: any) {
-    const form = new multiparty.Form();
-    const { fields, files } = await new Promise<any>((resolve, reject) => {
-        form.parse(req, (err: Error, fields: any, files: any) => {
-            if (err) reject(err);
-            resolve({fields: fields, files: files});
-        });
-    });
+export async function POST(req: NextRequest) {
+    const data = await req.formData();
+    // const { fields, files } = await new Promise<any>((resolve, reject) => {
+    //     form.parse(req, (err: Error, fields: any, files: any) => {
+    //         if (err) reject(err);
+    //         resolve({fields: fields, files: files});
+    //     });
+    // });
 
-    console.log("length: ", files.files.length);
-    console.log("files: ", files);
-    console.log(fields);
+    // console.log("length: ", files.files.length);
+    // console.log("files: ", files);
+    // console.log(fields);
+
+
+    console.log(data);
 
     return new Response();
 }

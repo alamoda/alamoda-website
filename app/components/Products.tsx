a1f2ba5fba7690d32886d0a79db69c05f6c9400e
simozampa <simozampa@ucla.edu>
2023-05-18 11:58:10 -0700

    Display first image on products page

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -75,7 +75,7 @@ export default function Products() {
             <Link key={product._id} href={"dashboard/products/" + product._id} className="group">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                 <img
-                  src={'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg'}
+                  src={product.images[0] != null ? product.images[0] : 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg'}
                   alt={'Image Alt'}
                   className="h-full w-full object-cover object-center group-hover:opacity-75"
                 />

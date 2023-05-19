eb17029c775ae55609ef4daffc15ac3c7bcfeeb4
simozampa <simozampa@ucla.edu>
2023-05-19 16:29:26 -0700

    Encrypt password in User Model

--- app/models/User.ts
+++ app/models/User.ts
@@ -1,8 +1,17 @@
 import { Schema, models, model } from "mongoose";
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
 });
+
+UserSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) {
+        next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+})
 
 export const User = models.User || model('User', UserSchema);

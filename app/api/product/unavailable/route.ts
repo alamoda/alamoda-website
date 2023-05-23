b423776b8d8c16c56675eacb81c1cb4b9ed89d30
pperuzzo <pierlorenzo.peruzzo@gmail.com>
2023-05-23 09:30:39 -0700

    Mark products as unavailable

--- app/api/product/unavailable/route.ts
+++ app/api/product/unavailable/route.ts
@@ -8,13 +8,14 @@ export async function POST(req: Request) {
         cutoff
     } = await req.json();
 
-    const products = await Product.find({ available: true, updated_at: { $lt: cutoff } });
-
-    console.log(products.length)
-
-    // const productDoc = await Product.create({
-
-    // });
-
-    return new Response(JSON.stringify({ "message": "success" }));
+    try {
+        const result = await Product.updateMany(
+            { available: true, updated_at: { $lt: cutoff } },
+            { available: false }
+        );
+        
+        return new Response(JSON.stringify({ "status": "success", "message": result }));
+    } catch (err) {
+        return new Response(JSON.stringify({ "status": "error", "message": err }));
+    }
 }

7faa4f188241c3f8f2bf916ff1c40ae82b5ec55d
simozampa <simozampa@ucla.edu>
2023-05-27 15:56:57 -0700

    Add categories and genders in constants.ts

---
+++ app/utils/constants.ts
@@ -0,0 +1,13 @@
+export const CATEGORIES = {
+    BAGS: 'BAGS',
+    CLOTHING: 'CLOTHING',
+    FOOTWEAR: 'FOOTWEAR',
+    ACCESSORIES: 'ACCESSORIES',
+    JEWELLERY: 'JEWELLERY',
+} as const;
+
+export const GENDERS = {
+    MAN: 'MAN',
+    WOMAN: 'WOMAN',
+    UNISEX: 'UNISEX',
+} as const;

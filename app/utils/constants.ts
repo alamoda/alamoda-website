d218f6666967909dccdb4221f590d96bbee28544
pperuzzo <pierlorenzo.peruzzo@gmail.com>
2023-06-01 10:44:16 -0700

    edit constants

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -6,20 +6,108 @@
 //     JEWELLERY: 'JEWELLERY',
 // } as const;
 
-export const CATEGORIES = [
+
+
+
+// ACCESSORIES (326)
+// BEAUTY CASES (23)
+// BELTS e BRACES (39)
+// BOWTIES e TIES (2)
+// CLUTCHES (0)
+// COVERS e CASES (12)
+// CUFFLINKS (0)
+// EXTRA (2)
+// EXTRAS (15)
+// GLOVES (1)
+// HATS e HAIRBANDS (59)
+// KEY RINGS (12)
+// SCARVES (17)
+// SHOULDER BAGS (0)
+// SOCKS (3)
+// SUNGLASSES (3)
+// WALLETS (138)
+// WALLETS (0)
+
+
+// BAGS (135)
+// BACKPACKS (20)
+// BRIEFCASES (0)
+// BUCKET BAGS (1)
+// CLUTCHES (15)
+// HANDBAGS (25)
+// SHOULDER BAGS (71)
+// TRAVEL BAGS (3)
+
+
+// CLOTHING (1983)
+// BERMUDA SHORTS (100)
+// BLAZERS e VESTS (16)
+// COATS (34)
+// DOWN JACKETS (31)
+// FURS (0)
+// JACKETS (179)
+// JEANS (105)
+// KNITWEAR (175)
+// LEATHER JACKETS (6)
+// LEGGINGS (3)
+// PANTS (284)
+// POLO SHIRTS (66)
+// SHIRTS (204)
+// SOCKS (0)
+// SUITS (25)
+// SWEATSHIRTS (278)
+// SWIMWEAR (56)
+// T-SHIRTS (408)
+// TOP (7)
+// TRENCH COATS (3)
+// UNDERWEAR (3)
+
+
+
+// Footwear (376 items)
+// Boots (32 items)
+// Espadrilles (1 item)
+// Lace Up (22 items)
+// Loafers (47 items)
+// Sandals (48 items)
+// Sneakers (226 items)
+
+
+// Jewellery (42 items)
+// Bracelets (24 items)
+// Extras (1 item)
+// Necklaces (3 items)
+// Rings (14 items)
+
+
+
+
+
+
+
+
+
+
+export const DEPARTMENTS = [
 
     // Women
     {
         id: 'WOMEN',
         name: 'Women',
         href: '',
-        sections: [
+        categories: [
             {
                 id: 'BAGS',
                 name: 'Bags',
                 href: '',
                 imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
-                imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
+                imageAlt: '.',
+                subcategories: [
+                    {
+                        id: '',
+                        name: '',
+                    }
+                ]
             },
             {
                 id: 'CLOTHING',
@@ -34,6 +122,12 @@ export const CATEGORIES = [
                 href: '',
                 imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
                 imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
+                subcategories: [
+                    {
+                        id: 'sneakers',
+                        name: 'Sneakers'
+                    }
+                ]
             },
             {
                 id: 'ACCESSORIES',
@@ -134,8 +228,7 @@ export const CATEGORIES = [
 ];
 
 export const GENDERS = {
-    MAN: 'MAN',
-    WOMAN: 'WOMAN',
-    UNISEX: 'UNISEX',
+    MAN: 'MEN',
+    WOMAN: 'WOMEN',
     LIFESTYLE: 'LIFESTYLE'
 } as const;

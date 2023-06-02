e62c1da4ca110692b7991106ebea2b9ea36100b0
pperuzzo <pierlorenzo.peruzzo@gmail.com>
2023-06-02 13:46:24 -0700

    Fixes:

--- app/shop/[[...slug]]/page.tsx
+++ app/shop/[[...slug]]/page.tsx
@@ -1,3 +1,10 @@
+import Breadcrumb from '@/app/components/Breadcrumb';
+import CircularButton from '@/app/components/CircularButton';
+import Pagination from '@/app/components/Pagination';
+import ProductCard from '@/app/components/ProductCard';
+import Link from 'next/link';
+
+
 async function getData(department: string | null, category: string | null, subcategory: string | null, skip: Number = 0) {
 
     const departementParam = department ? `department=${department}&` : ''
@@ -5,7 +12,7 @@ async function getData(department: string | null, category: string | null, subca
     const subcategoryParam = subcategory ? `subcategory=${subcategory}&` : ''
 
 
-    const res = await fetch(`http://localhost:3000/api/products?${departementParam}${categoryParam}${subcategoryParam}limit=10&skip=${skip}`)
+    const res = await fetch(`http://localhost:3000/api/products?${departementParam}${categoryParam}${subcategoryParam}limit=60&skip=${skip}`)
 
     if (!res.ok) {
         throw new Error('Failed to fetch data');
@@ -13,22 +20,31 @@ async function getData(department: string | null, category: string | null, subca
 
     return await res.json();
 }
+function classNames(...classes: any) {
+    return classes.filter(Boolean).join(' ')
+}
 
 export default async function Shop({ params }: { params: { slug: Array<string> } }) {
 
     const data = await getData(params.slug ? params.slug[0] : null, params.slug ? params.slug[1] : null, params.slug ? params.slug[2] : null);
 
+    const pages = []
+    if (params.slug && params.slug[0]) pages.push({name: params.slug[0], href: ""}) 
+    if (params.slug && params.slug[1]) pages.push({name: params.slug[1], href: ""}) 
+    if (params.slug && params.slug[2]) pages.push({name: params.slug[2], href: ""}) 
+
+
     return (
-        <div>
-            {data.count}
+        <div className="px-4 py-4">
+            <Breadcrumb pages={pages} />
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {data.products.map((product: any) => (
-                <div className="py-2">
-                    <h1 className="font-bold">{product.name}</h1>
-                    <p className="text-xm lowercase">{product.department}</p>
-                    <p className="text-xm lowercase">{product.category}</p>
-                    <p className="text-xm lowercase">{product.subcategory}</p>
-                </div>
+                <ProductCard key={product.mongo_id} product={product} />
             ))}
         </div>
-    );
+        <div className='mt-8'>
+            <Pagination pageCount={100} selectedPage={1}/>
+        </div>
+    </div>
+    )
 }

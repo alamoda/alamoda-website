0cca263c5c7beaf7ff7b1e02a1a9319a9479e534
simozampa <simozampa@ucla.edu>
2023-06-05 23:17:32 -0700

    Revert to old product form with new features

--- app/(components)/ProductForm.tsx
+++ app/(components)/ProductForm.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react";
-import { Option, Feature, Size, Brand } from "../(types)";
+import { Product, Option, Feature, Size, Subcategory, Department, Category } from "../(types)";
 import { useRouter } from "next/navigation";
 import { ReactSortable } from "react-sortablejs";
 import axios from "axios";
@@ -12,38 +12,44 @@ import PrimaryInput from "@/app/(components)/PrimaryInput";
 import TextAreaInput from "@/app/(components)/TextAreaInput";
 import Image from 'next/image';
 import PrimarySelect from "./PrimarySelect";
-import { Department } from "../(types)";
-import { Category } from "../(types)";
-import { Subcategory } from "../(types)";
-import { Product } from "../(types)";
 
-const ProductForm: React.FC<{ product?: Product; }> = ({ product }) => {
-    const [formProduct, setFormProduct] = useState<Product>({
-        mongo_id: '',
-        id: 0,
-        sku: '',
-        price: 0,
-        wholesale_price: 0,
-        available: false,
-        brand: { id: 0, name: 'None' },
-        name: '',
-        description: '',
-        features: undefined,
-        department: { id: 0, name: 'None', categories: [] },
-        category: { id: 0, name: 'None', subcategories: [] },
-        subcategory: { id: 0, name: 'None' },
-        images: [],
-        sizes: [],
-        status: 0,
-        updated_at: undefined,
-        created_at: undefined,
-    })
+const ProductForm = ({
+    mongo_id = '',
+    id: existingId = 0,
+    sku: existingSku = '',
+    brand: existingBrand = { id: 0, name: 'Brand' },
+    name: existingName = '',
+    description: existingDescription = '',
+    price: existingPrice = 0,
+    wholesale_price: existingWholesaleprice = 0,
+    available: existingAvailable = true,
+    department: existingDepartment = { id: 0, name: 'None', categories: [] },
+    category: existingCategory = { id: 0, name: 'None', subcategories: [] },
+    subcategory: existingSubcategory = { id: 0, name: 'None' },
+    features: existingFeatures = [],
+    sizes: existingSizes = [],
+    images: existingImages = [],
+    status: existingStatus = 0,
+}) => {
+    const [id, setId] = useState<number>(existingId);
+    const [sku, setSku] = useState<string>(existingSku);
+    const [name, setName] = useState<string>(existingName);
+    const [brand, setBrand] = useState<Option>(existingBrand);
+    const [description, setDescription] = useState<string>(existingDescription);
+    const [price, setPrice] = useState<number>(existingPrice);
+    const [wholesale_price, setWholesaleprice] = useState<number>(existingWholesaleprice);
+    const [available, setAvailable] = useState<boolean>(existingAvailable);
+    const [department, setDepartment] = useState<Department>(existingDepartment);
+    const [category, setCategory] = useState<Category>(existingCategory);
+    const [subcategory, setSubcategory] = useState<Subcategory>(existingSubcategory);
+    const [features, setFeatures] = useState<Feature[]>(existingFeatures);
+    const [sizes, setSizes] = useState<Size[]>(existingSizes);
+    const [images, setImages] = useState<string[]>(existingImages);
+    const [status, setStatus] = useState<number>(existingStatus);
 
-    const [images, setImages] = useState<string[]>([]);
     const [brands, setBrands] = useState<Option[]>([]);
     const [departments, setDepartments] = useState<Department[]>([]);
-    const [department, setDepartment] = useState<Department>();
-    const [category, setCategory] = useState<Category>();
+
 
     const router = useRouter();
 
@@ -52,13 +58,6 @@ const ProductForm: React.FC<{ product?: Product; }> = ({ product }) => {
         fetchDepartments();
     }, []);
 
-    useEffect(() => {
-        if (product) {
-            setFormProduct(product);
-            setImages(product.images);
-        }
-    }, [product]);
-
     async function fetchBrands() {
         const res = await axios.get('/api/brands');
         setBrands(res.data);
@@ -66,105 +65,49 @@ const ProductForm: React.FC<{ product?: Product; }> = ({ product }) => {
 
     async function fetchDepartments() {
         const res = await axios.get('/api/department');
-        setBrands(res.data);
+        setDepartments(res.data);
     }
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = event.target;
-        setFormProduct((prevProduct) => ({
-            ...prevProduct,
-            [name]: value,
-        }));
-    };
-
-    const handleDepartmentChange = (value: Department) => {
-        setFormProduct((prevProduct) => ({
-            ...prevProduct,
-            ['department']: value,
-        }));
-    };
-
-    const handleCategoryChange = (value: Category) => {
-        setFormProduct((prevProduct) => ({
-            ...prevProduct,
-            ['category']: value,
-        }));
-    };
-
-    const handleSubcategoryChange = (value: Subcategory) => {
-        setFormProduct((prevProduct) => ({
-            ...prevProduct,
-            ['subcategory']: value,
-        }));
-    };
-
-    const handleBrandChange = (value: Brand) => {
-        setFormProduct((prevProduct) => ({
-            ...prevProduct,
-            ['brand']: value,
-        }));
-    };
-
-    const handleAvailabilityChange = (value: any) => {
-        setFormProduct((prevProduct) => ({
-            ...prevProduct,
-            ['available']: value,
-        }));
-    };
-
-    const handleStatusChange = (value: any) => {
-        setFormProduct((prevProduct) => ({
-            ...prevProduct,
-            ['status']: value,
-        }));
-    };
-
-    const handleSelectChange = (value: Option | Department | Category | Subcategory, propertyName: string) => {
-        setFormProduct((prevProduct) => ({
-            ...prevProduct,
-            [propertyName]: value,
-        }));
-    };
-
     function createOrUpdateProduct() {
-        // if (mongo_id) {
-        //     axios.put('/api/product', {
-        //         mongo_id,
-        //         id,
-        //         sku,
-        //         brand_name: brand.name,
-        //         name,
-        //         description,
-        //         price,
-        //         wholesale_price,
-        //         available: Boolean(available),
-        //         department,
-        //         category,
-        //         subcategory,
-        //         features,
-        //         sizes,
-        //         images,
-        //         status
-        //     });
-        // } else {
-        //     axios.post('/api/product', {
-        //         id,
-        //         sku,
-        //         brand_name: brand.name,
-        //         name,
-        //         description,
-        //         price,
-        //         wholesale_price,
-        //         available: Boolean(available),
-        //         department,
-        //         category,
-        //         subcategory,
-        //         features,
-        //         sizes,
-        //         images,
-        //         status
-        //     });
-        //     router.push('/dashboard/');
+        if (mongo_id) {
+            axios.put('/api/product', {
+                mongo_id,
+                id,
+                sku,
+                brand_name: brand.name,
+                name,
+                description,
+                price,
+                wholesale_price,
+                available: Boolean(available),
+                department,
+                category,
+                subcategory,
+                features,
+                sizes,
+                images,
+                status
+            });
+        } else {
+            axios.post('/api/product', {
+                id,
+                sku,
+                brand_name: brand.name,
+                name,
+                description,
+                price,
+                wholesale_price,
+                available: Boolean(available),
+                department,
+                category,
+                subcategory,
+                features,
+                sizes,
+                images,
+                status
+            });
+            router.push('/dashboard/');
+        }
     }
 
     // Reads the file and sends it to the backend
@@ -223,72 +166,77 @@ const ProductForm: React.FC<{ product?: Product; }> = ({ product }) => {
     return (
         <div className="px-16">
             {/* IMAGES */}
-            {/* <div className="flex items-center gap-4">
-            <div className="flex flex-wrap items-center gap-4">
-                <PhotoInput text="Product Images" onChange={uploadImages} />
-                <ReactSortable list={itemObjects} setList={(newItems) => setImages(newItems.map((item) => item.url))}>
-                    {!!itemObjects?.length && itemObjects.map(item => (
-                        <div key={item.id} className="inline-block mx-4">
-                            <Image src={item.url} alt={item.id} width={217} height={290} />
-                        </div>
-                    ))}
-                </ReactSortable>
+            <div className="flex items-center gap-4">
+                <div className="flex flex-wrap items-center gap-4">
+                    <PhotoInput text="Product Images" onChange={uploadImages} />
+                    <ReactSortable list={itemObjects} setList={(newItems) => setImages(newItems.map((item) => item.url))}>
+                        {!!itemObjects?.length && itemObjects.map(item => (
+                            <div key={item.id} className="inline-block mx-4">
+                                <Image src={item.url} alt={item.id} width={217} height={290} />
+                            </div>
+                        ))}
+                    </ReactSortable>
+                </div>
+
             </div>
-        </div> */}
 
             <div className="gap-8 mt-8">
                 <div className="flex items-center gap-4">
                     {/* NAME */}
-                    <PrimaryInput size='w-56' name="name" label="Name" placeholder="Name" value={formProduct.name} onChangeMethod={handleInputChange} />
+                    <PrimaryInput size='w-56' label="Name" placeholder="Name" value={name} onChangeMethod={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)} />
                     {/* SKU */}
-                    <PrimaryInput size='w-56' name="sku" label="SKU" placeholder="SKU" value={formProduct.sku} onChangeMethod={handleInputChange} />
+                    <PrimaryInput size='w-56' label="SKU" placeholder="SKU" value={sku} onChangeMethod={(e: React.ChangeEvent<HTMLInputElement>) => setSku(e.target.value)} />
                     {/* ID */}
-                    <PrimaryInput size='w-32' name="id" label="Product ID" placeholder="ID" value={formProduct.id.toString()} onChangeMethod={handleInputChange} />
+                    <PrimaryInput size='w-32' label="Product ID" placeholder="ID" value={id.toString()} onChangeMethod={(e: React.ChangeEvent<HTMLInputElement>) => setId(Number(e.target.value))} />
                 </div>
                 <div className="flex items-center mt-4">
                     <div>
                         <div className="flex items-center gap-4">
                             {/* DEPARTMENT */}
                             <PrimarySelect
-                                value={departments.find(x => x.name === formProduct.department?.name) || { id: 0, name: 'None' }}
+                                value={department || { id: 0, name: 'None' }}
                                 options={departments}
-                                onValueChange={handleDepartmentChange}
+                                onValueChange={(value: Department) => setDepartment(value)}
                             />
                             {/* CATEGORY  */}
-                            {department && <PrimarySelect
-                                value={department?.categories.find(x => x.name === formProduct.category?.name) || { id: 0, name: 'None' }}
-                                options={department.categories}
-                                onValueChange={handleCategoryChange}
-                            />}
+                            {department &&
+                                <PrimarySelect
+                                    value={category || { id: 0, name: 'None' }}
+                                    options={department.categories}
+                                    onValueChange={(value: Category) => setCategory(value)}
+                                />
+                            }
                             {/* SUBCATEGORY  */}
-                            {category && <PrimarySelect
-                                value={category?.subcategories.find(x => x.name === formProduct.subcategory?.name) || { id: 0, name: 'None' }}
-                                options={category.subcategories}
-                                onValueChange={handleSubcategoryChange}
-                            />}
+                            {category &&
+                                <PrimarySelect
+                                    value={subcategory || { id: 0, name: 'None' }}
+                                    options={category.subcategories}
+                                    onValueChange={(value: Option) => setSubcategory(value)}
+                                />
+                            }
                         </div>
                         <div className="flex flex-wrap items-center gap-4 mt-4">
                             {/* BRAND */}
-                            <PrimarySelect value={formProduct.brand} options={brands} onValueChange={handleBrandChange} />
+                            <PrimarySelect value={brand} options={brands} onValueChange={(value: Option) => setBrand(value)} />
                             {/* AVAILABILITY */}
                             <PrimarySelect
-                                value={{ id: 0, name: 'true' }}
+                                value={available ? {id: 1, name: available.toString()} : { id: 0, name: 'None:'}}
                                 options={[{ id: 1, name: 'true' }, { id: 2, name: 'false' }]}
-                                onValueChange={handleAvailabilityChange}
+                                onValueChange={(value: Option) => setAvailable(Boolean(value.name))}
                             />
                             {/* STATUS */}
                             <PrimarySelect
                                 value={{ id: 0, name: 'Status: ' + status.toString() }}
                                 options={[{ id: 1, name: '-1' }, { id: 2, name: '0' }, { id: 3, name: '1' }, { id: 4, name: '2' }]}
-                                onValueChange={handleStatusChange}
+                                onValueChange={(value: Option) => setStatus(Number(value.name))}
                             />
                             {/* SIZES */}
-                            {formProduct.sizes.length > 0 &&
+                            {sizes.length > 0 &&
                                 <div>
                                     <span className="font-medium text-sm">
                                         Sizes:
                                     </span>
-                                    {formProduct.sizes.map((size, index) => (
+                                    {sizes.map((size, index) => (
                                         <span key={index} className="text-sm">
                                             {" " + size.name}
                                         </span>
@@ -303,29 +251,27 @@ const ProductForm: React.FC<{ product?: Product; }> = ({ product }) => {
 
             {/* DESCRIPTION */}
             < div className="flex items-center justify-between mt-2" >
-                <TextAreaInput label="Description" value={formProduct.description} onChange={handleInputChange} />
+                <TextAreaInput label="Description" value={description} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setDescription(e.target.value)} />
             </div >
 
             {/* FEATURES */}
-            {formProduct.features &&
-                <div className="flex flex-wrap gap-4 text-sm mt-2" >
-                    {Array.from(formProduct.features).map((feature, index) => (
-                        <div key={index}>
-                            <div className="font-medium mb-1">
-                                {feature.name}
-                            </div>
-                            <div className="border border-gray-300 rounded-lg px-2 py-2 truncate...">
-                                {feature.value}
-                            </div>
+            <div className="flex flex-wrap gap-4 text-sm mt-2" >
+                {Array.from(features).map((feature, index) => (
+                    <div key={index}>
+                        <div className="font-medium mb-1">
+                            {feature.name}
                         </div>
-                    ))}
-                </div>
-            }
-            <div className="flex items-center gap-4 mt-2" >
+                        <div className="border border-gray-300 rounded-lg px-2 py-2 truncate...">
+                            {feature.value}
+                        </div>
+                    </div>
+                ))}
+            </div>
+            < div className="flex items-center gap-4 mt-2" >
                 {/* PRICE */}
-                <PriceInput name="Price" value={formProduct.price} onChange={handleInputChange} />
+                <PriceInput name="Price" value={price} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPrice(Number(e.target.value))} />
                 {/* WHOLESALE PRICE */}
-                <PriceInput name="Wholesale Price" value={formProduct.wholesale_price} onChange={handleInputChange} />
+                <PriceInput name="Wholesale Price" value={wholesale_price} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setWholesaleprice(Number(e.target.value))} />
             </div>
 
             <div className="mt-2">
--- app/dashboard/products/[department]/[product]/page.tsx
+++ app/dashboard/products/[department]/[product]/page.tsx
@@ -14,26 +14,7 @@ export default function Page({
 }: {
     params: { product: string, department: string };
 }) {
-    const [product, setProduct] = useState<Product>({
-        mongo_id: '',
-        id: 0,
-        sku: '',
-        price: 0,
-        wholesale_price: 0,
-        available: false,
-        brand: { id: 0, name: 'None' },
-        name: '',
-        description: '',
-        features: undefined,
-        department: { id: 0, name: 'None', categories: [] },
-        category: { id: 0, name: 'None', subcategories: [] },
-        subcategory: { id: 0, name: 'None' },
-        images: [],
-        sizes: [],
-        status: 0,
-        updated_at: undefined,
-        created_at: undefined,
-    });
+    const [product, setProduct] = useState({});
 
     const router = useRouter();
 
@@ -83,7 +64,7 @@ export default function Page({
             {/* EDIT PRODUCT */}
             {Object.keys(product).length > 0 ?
                 <>
-                    <ProductForm product={product} />
+                    <ProductForm {...product} />
                     <span className="px-16">
                         <DeleteButton text="delete" onClick={deleteProduct} />
                     </span>

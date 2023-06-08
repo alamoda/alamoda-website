3a0072e0e02efe04eae33316c7f17d51141822df
simozampa <simozampa@ucla.edu>
2023-06-07 18:40:04 -0700

    change image on hover in product card

--- app/(components)/ProductCard.tsx
+++ app/(components)/ProductCard.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client'
+
+import React, { useRef, useState } from 'react';
 import Link from "next/link"
 import { Product } from '@/app/(types)/index';
 import Image from 'next/image';
@@ -9,16 +11,20 @@ interface ComponentProps {
 }
 
 const ProductCard: React.FC<ComponentProps> = ({ product, route }) => {
+  const [image, setImage] = useState(product.images[0]);
+
   return (
     <Link key={product.mongo_id} href={route + '/' + product.mongo_id} className="group flex flex-col justify-end">
       <div className="flex flex-col justify-center items-center">
         <div>
           <Image
-            src={product.images.length > 0 ? product.images[0] : '/'}
+            src={product.images.length > 0 ? image : '/'}
             alt={product.name}
             width={217}
             height={290}
-            className="w-full h-full object-cover object-center group-hover:opacity-75"
+            className="w-full h-full object-cover object-center"
+            onMouseEnter={() => setImage(product.images[1])}
+            onMouseLeave={() => setImage(product.images[0])}
           />
         </div>
 

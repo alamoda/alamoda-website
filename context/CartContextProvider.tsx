42dc19cf163fb62a8e9ad22dd5138d86e387b6e8
simozampa <simozampa@ucla.edu>
2023-06-07 23:30:26 -0700

    Set cartProduct in localStorage everytime its updated

--- context/CartContextProvider.tsx
+++ context/CartContextProvider.tsx
@@ -1,15 +1,24 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { CartContext } from "./CartContext"
 import { Product } from "@/app/(types)"
 
+function getInitialState() {
+    const cartProducts = localStorage.getItem('cartProducts')
+    return cartProducts ? JSON.parse(cartProducts) : []
+  }
+  
 export function CartContextProvider({
     children,
 }: {
     children: React.ReactNode
 }) {
-    const [cartProducts, setCartProducts] = useState<Product[]>([]);
+    const [cartProducts, setCartProducts] = useState<Product[]>(getInitialState);
+
+    useEffect(() => {
+        localStorage.setItem('cartProducts', JSON.stringify(cartProducts))
+      }, [cartProducts])
 
     return (
         <CartContext.Provider value={{ cartProducts, setCartProducts }}>

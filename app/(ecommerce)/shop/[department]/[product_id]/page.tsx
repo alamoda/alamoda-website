071b2fea1f5a5e02136118a9e16fa059b384728d
simozampa <simozampa@ucla.edu>
2023-06-10 22:36:45 -0700

    Fix color buttons in the product page and cart page

--- app/(ecommerce)/shop/[department]/[product_id]/page.tsx
+++ app/(ecommerce)/shop/[department]/[product_id]/page.tsx
@@ -208,9 +208,8 @@ export default function Page({ params }: { params: { product_id: string } }) {
                           value={size}
                           className={({ active, checked }) =>
                             classNames(
-                              active ? 'ring-2 ring-indigo-500 ring-offset-2' : '',
                               checked
-                                ? 'border-transparent bg-indigo-600 text-white hover:bg-indigo-700'
+                                ? 'border-transparent bg-gray-900 text-white hover:bg-gray-800'
                                 : 'border-gray-200 bg-white text-gray-900 hover:bg-gray-50',
                               'cursor-pointer focus:outline-none flex items-center justify-center rounded-md border py-3 px-3 text-sm font-medium uppercase sm:flex-1'
                             )
@@ -225,7 +224,7 @@ export default function Page({ params }: { params: { product_id: string } }) {
 
                 <button
                   onClick={handleAddToCart}
-                  className="mt-8 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="mt-8 flex w-full items-center justify-center rounded-md border border-transparent bg-gray-900 px-8 py-3 text-base font-medium text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                   Add to cart
                 </button>

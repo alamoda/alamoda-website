77ecbb64fe7f57424b6cc289e4676268a677f612
pperuzzo <pierlorenzo.peruzzo@gmail.com>
2023-06-21 21:58:49 -0700

    Shop page

--- app/(ecommerce)/shop/page.tsx
+++ app/(ecommerce)/shop/page.tsx
@@ -1,30 +1,5 @@
-'use client'
-
-import { useState } from 'react'
 import ProductList from '@/app/(components)/ProductList'
 
-const offers = [
-    { name: 'Download the app', description: 'Get an exclusive $5 off code', href: '#' },
-    { name: "Return when you're ready", description: '60 days of free returns', href: '#' },
-    { name: 'Sign up for our newsletter', description: '15% off your first order', href: '#' },
-]
-const trendingProducts = [
-    {
-        id: 1,
-        name: 'Machined Pen',
-        color: 'Black',
-        price: '$35',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-product-01.jpg',
-        imageAlt: 'Black machined steel pen with hexagonal grip and small white logo at top.',
-        availableColors: [
-            { name: 'Black', colorBg: '#111827' },
-            { name: 'Brass', colorBg: '#FDE68A' },
-            { name: 'Chrome', colorBg: '#E5E7EB' },
-        ],
-    },
-    // More products...
-]
 const collections = [
     {
         name: 'Desk and Office',
@@ -48,6 +23,7 @@ const collections = [
         href: '#',
     },
 ]
+
 const testimonials = [
     {
         id: 1,
@@ -68,71 +44,8 @@ const testimonials = [
         attribution: 'Chris Paul, Phoenix',
     },
 ]
-const footerNavigation = {
-    products: [
-        { name: 'Bags', href: '#' },
-        { name: 'Tees', href: '#' },
-        { name: 'Objects', href: '#' },
-        { name: 'Home Goods', href: '#' },
-        { name: 'Accessories', href: '#' },
-    ],
-    customerService: [
-        { name: 'Contact', href: '#' },
-        { name: 'Shipping', href: '#' },
-        { name: 'Returns', href: '#' },
-        { name: 'Warranty', href: '#' },
-        { name: 'Secure Payments', href: '#' },
-        { name: 'FAQ', href: '#' },
-        { name: 'Find a store', href: '#' },
-    ],
-    company: [
-        { name: 'Who we are', href: '#' },
-        { name: 'Sustainability', href: '#' },
-        { name: 'Press', href: '#' },
-        { name: 'Careers', href: '#' },
-        { name: 'Terms & Conditions', href: '#' },
-        { name: 'Privacy', href: '#' },
-    ],
-    legal: [
-        { name: 'Terms of Service', href: '#' },
-        { name: 'Return Policy', href: '#' },
-        { name: 'Privacy Policy', href: '#' },
-        { name: 'Shipping Policy', href: '#' },
-    ],
-    bottomLinks: [
-        { name: 'Accessibility', href: '#' },
-        { name: 'Privacy', href: '#' },
-        { name: 'Terms', href: '#' },
-    ],
-}
 
-const perks = [
-    {
-        name: 'Free returns',
-        imageUrl: 'https://tailwindui.com/img/ecommerce/icons/icon-returns-light.svg',
-        description: 'Not what you expected? Place it back in the parcel and attach the pre-paid postage stamp.',
-    },
-    {
-        name: 'Same day delivery',
-        imageUrl: 'https://tailwindui.com/img/ecommerce/icons/icon-calendar-light.svg',
-        description:
-            'We offer a delivery service that has never been done before. Checkout today and receive your products within hours.',
-    },
-    {
-        name: 'All year discount',
-        imageUrl: 'https://tailwindui.com/img/ecommerce/icons/icon-gift-card-light.svg',
-        description: 'Looking for a deal? You can use the code "ALLYEAR" at checkout and get money off all year round.',
-    },
-    {
-        name: 'For the planet',
-        imageUrl: 'https://tailwindui.com/img/ecommerce/icons/icon-planet-light.svg',
-        description: 'We’ve pledged 1% of sales to the preservation and restoration of the natural environment.',
-    },
-]
-
-
-export default function Example() {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+export default function Page() {
 
     return (
         <div className="bg-white">
@@ -140,86 +53,98 @@ export default function Example() {
             <main>
                 {/* Hero section */}
                 <div className="relative">
-                    {/* Background image and overlap */}
-                    <div aria-hidden="true" className="absolute inset-0 hidden sm:flex sm:flex-col">
-                        <div className="relative w-full flex-1 bg-gray-800">
-                            <div className="absolute inset-0 overflow-hidden">
-                                <img
-                                    src="https://tailwindui.com/img/ecommerce-images/home-page-04-hero-full-width.jpg"
-                                    alt=""
-                                    className="h-full w-full object-cover object-center"
-                                />
-                            </div>
-                            <div className="absolute inset-0 bg-gray-900 opacity-50" />
-                        </div>
-                        <div className="h-32 w-full bg-white md:h-40 lg:h-48" />
-                    </div>
-
-                    <div className="relative mx-auto max-w-3xl px-4 pb-96 text-center sm:px-6 sm:pb-0 lg:px-8">
-                        {/* Background image and overlap */}
-                        <div aria-hidden="true" className="absolute inset-0 flex flex-col sm:hidden">
-                            <div className="relative w-full flex-1 bg-gray-800">
-                                <div className="absolute inset-0 overflow-hidden">
-                                    <img
-                                        src="https://tailwindui.com/img/ecommerce-images/home-page-04-hero-full-width.jpg"
-                                        alt=""
-                                        className="h-full w-full object-cover object-center"
-                                    />
-                                </div>
-                                <div className="absolute inset-0 bg-gray-900 opacity-50" />
-                            </div>
-                            <div className="h-48 w-full bg-white" />
-                        </div>
-                        <div className="relative py-32">
-                            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl">Mid-Season Sale</h1>
-                            <div className="mt-4 sm:mt-6">
-                                <a
-                                    href="#"
-                                    className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 font-medium text-white hover:bg-indigo-700"
-                                >
-                                    Shop Collection
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <section aria-labelledby="collection-heading" className="relative -mt-96 sm:mt-0">
+                    <section aria-labelledby="collection-heading" className="relative pt-24">
                         <h2 id="collection-heading" className="sr-only">
                             Collections
                         </h2>
                         <div className="mx-auto grid max-w-md grid-cols-1 gap-y-6 px-4 sm:max-w-7xl sm:grid-cols-3 sm:gap-x-6 sm:gap-y-0 sm:px-6 lg:gap-x-8 lg:px-8">
-                            {collections.map((collection) => (
-                                <div
-                                    key={collection.name}
-                                    className="group relative h-96 rounded-lg bg-white shadow-xl sm:aspect-h-5 sm:aspect-w-4 sm:h-auto"
-                                >
-                                    <div>
-                                        <div aria-hidden="true" className="absolute inset-0 overflow-hidden rounded-lg">
-                                            <div className="absolute inset-0 overflow-hidden group-hover:opacity-75">
-                                                <img
-                                                    src={collection.imageSrc}
-                                                    alt={collection.imageAlt}
-                                                    className="h-full w-full object-cover object-center"
-                                                />
-                                            </div>
-                                            <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50" />
+                            <div
+                                className="group relative h-96 rounded-lg bg-white shadow-xl sm:aspect-h-5 sm:aspect-w-4 sm:h-auto"
+                            >
+                                <div>
+                                    <div aria-hidden="true" className="absolute inset-0 overflow-hidden rounded-lg">
+                                        <div className="absolute inset-0 overflow-hidden group-hover:opacity-75">
+                                            <img
+                                                src="https://www.baseblu.com/img/cms/home/donna06.jpg"
+                                                alt="Women collection"
+                                                className="h-full w-full object-cover object-center"
+                                            />
                                         </div>
-                                        <div className="absolute inset-0 flex items-end rounded-lg p-6">
-                                            <div>
-                                                <p aria-hidden="true" className="text-sm text-white">
-                                                    Shop the collection
-                                                </p>
-                                                <h3 className="mt-1 font-semibold text-white">
-                                                    <a href={collection.href}>
-                                                        <span className="absolute inset-0" />
-                                                        {collection.name}
-                                                    </a>
-                                                </h3>
-                                            </div>
+                                        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50" />
+                                    </div>
+                                    <div className="absolute inset-0 flex items-end rounded-lg p-6">
+                                        <div>
+                                            <p aria-hidden="true" className="text-sm text-white">
+                                                Shop the collection
+                                            </p>
+                                            <h3 className="mt-1 font-semibold text-white">
+                                                <a href="shop/women">
+                                                    <span className="absolute inset-0" />
+                                                    Women
+                                                </a>
+                                            </h3>
                                         </div>
                                     </div>
                                 </div>
-                            ))}
+                            </div>
+                            <div
+                                className="group relative h-96 rounded-lg bg-white shadow-xl sm:aspect-h-5 sm:aspect-w-4 sm:h-auto"
+                            >
+                                <div>
+                                    <div aria-hidden="true" className="absolute inset-0 overflow-hidden rounded-lg">
+                                        <div className="absolute inset-0 overflow-hidden group-hover:opacity-75">
+                                            <img
+                                                src="https://www.baseblu.com/img/cms/home/uomo06.jpg"
+                                                alt="Men collection"
+                                                className="h-full w-full object-cover object-center"
+                                            />
+                                        </div>
+                                        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50" />
+                                    </div>
+                                    <div className="absolute inset-0 flex items-end rounded-lg p-6">
+                                        <div>
+                                            <p aria-hidden="true" className="text-sm text-white">
+                                                Discover
+                                            </p>
+                                            <h3 className="mt-1 font-semibold text-white">
+                                                <a href="shop/men">
+                                                    <span className="absolute inset-0" />
+                                                    Men
+                                                </a>
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                className="group relative h-96 rounded-lg bg-white shadow-xl sm:aspect-h-5 sm:aspect-w-4 sm:h-auto"
+                            >
+                                <div>
+                                    <div aria-hidden="true" className="absolute inset-0 overflow-hidden rounded-lg">
+                                        <div className="absolute inset-0 overflow-hidden group-hover:opacity-75">
+                                            <img
+                                                src="https://luxurylivinggroup.com/wp-content/uploads/2021/11/Versace-Home-SuperSalone-2021_22-1.jpg"
+                                                alt="Lifestyle collection"
+                                                className="h-full w-full object-cover object-center"
+                                            />
+                                        </div>
+                                        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50" />
+                                    </div>
+                                    <div className="absolute inset-0 flex items-end rounded-lg p-6">
+                                        <div>
+                                            <p aria-hidden="true" className="text-sm text-white">
+                                                Shop the collection
+                                            </p>
+                                            <h3 className="mt-1 font-semibold text-white">
+                                                <a href="shop/lifestyle">
+                                                    <span className="absolute inset-0" />
+                                                    Lifestyle
+                                                </a>
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </section>
                 </div>
@@ -227,7 +152,6 @@ export default function Example() {
                 {/* Preview */}
                 <section aria-labelledby="trending-heading">
                     <div className="mx-auto max-w-7xl px-4 py-24 sm:px-6 sm:py-32 lg:px-8 lg:pt-32">
-
 
                         <div className="">
                             {/* @ts-expect-error Server Component */}

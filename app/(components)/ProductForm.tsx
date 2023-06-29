23fe1b76cdf2f31985cf150216f507157d6460bf
simozampa <simozampa@ucla.edu>
2023-06-28 22:46:25 -0700

    Remove images from dashboard feature

--- app/(components)/ProductForm.tsx
+++ app/(components)/ProductForm.tsx
@@ -170,9 +170,11 @@ const ProductForm = ({
                     <ReactSortable list={itemObjects} setList={(newItems) => setImages(newItems.map((item) => item.url))}>
                         {!!itemObjects?.length && itemObjects.map(item => (
                             <div key={item.id} className="relative inline-block mx-4">
-                                <button className="absolute top-0 right-0 text-red-700">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                <button
+                                    className="absolute top-0 right-0 text-red-700"
+                                    onClick={() => setImages(images.filter(x => x !== item.url))}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                     </svg>
                                 </button>
                                 <Image src={item.url} alt={item.id} width={217} height={290} />

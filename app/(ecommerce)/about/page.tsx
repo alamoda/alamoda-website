3d452c7c1af6f8075396e38dd6ca9acc5c8b0005
pperuzzo <pierlorenzo.peruzzo@gmail.com>
2023-07-01 15:35:37 -0700

    Fixes

--- app/(ecommerce)/about/page.tsx
+++ app/(ecommerce)/about/page.tsx
@@ -1,66 +1,4 @@
-import Link from "next/link";
 import Image from "next/image";
-
-const posts = [
-    {
-        id: 1,
-        title: 'Boost your conversion rate',
-        href: '#',
-        description:
-            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-        imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-        category: { title: 'Marketing', href: '#' },
-        author: {
-            name: 'Michael Foster',
-            role: 'Co-Founder / CTO',
-            href: '#',
-            imageUrl:
-                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-    },
-    {
-        id: 2,
-        title: 'Boost your conversion rate',
-        href: '#',
-        description:
-            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-        imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-        category: { title: 'Marketing', href: '#' },
-        author: {
-            name: 'Michael Foster',
-            role: 'Co-Founder / CTO',
-            href: '#',
-            imageUrl:
-                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-    },
-    {
-        id: 3,
-        title: 'Boost your conversion rate',
-        href: '#',
-        description:
-            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-        imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-        category: { title: 'Marketing', href: '#' },
-        author: {
-            name: 'Michael Foster',
-            role: 'Co-Founder / CTO',
-            href: '#',
-            imageUrl:
-                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-    }
-]
-
 
 export default function Page() {
     return (
@@ -252,8 +190,8 @@ export default function Page() {
                 </div>
             </div >
 
-            {/* Blog Section */}
-            < div className="bg-white py-16 sm:py-24" >
+            {/* Blog Section - TODO */}
+            {/* < div className="bg-white py-16 sm:py-24" >
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl text-center">
                         <h2 className="text-4xl tracking-tight text-gray-900">From the blog</h2>
@@ -318,7 +256,7 @@ export default function Page() {
                         ))}
                     </div>
                 </div>
-            </div >
+            </div > */}
         </>
 
     )
--- app/(ecommerce)/shop/page.tsx
+++ app/(ecommerce)/shop/page.tsx
@@ -1,4 +1,5 @@
 import ProductList from '@/app/(components)/ProductList'
+import Testimonials from '@/app/(components)/Testimonials'
 import Link from 'next/link'
 
 const collections = [
@@ -151,7 +152,7 @@ export default function Page() {
 
                 {/* Preview */}
                 <section aria-labelledby="trending-heading">
-                    <div className="mx-auto max-w-7xl px-4 py-24 sm:px-6 sm:py-32 lg:px-8 lg:pt-32">
+                    <div className="mx-auto max-w-7xl px-4 pt-24 sm:px-6 sm:pt-32 lg:px-8 lg:pt-32">
 
                         <div className="">
                             {/* @ts-expect-error Server Component */}
@@ -188,80 +189,8 @@ export default function Page() {
                     </div>
                 </section>
 
-                {/* Sale and testimonials */}
-                <div className="relative overflow-hidden">
-                    {/* Decorative background image and gradient */}
-                    <div aria-hidden="true" className="absolute inset-0">
-                        <div className="absolute inset-0 mx-auto max-w-7xl overflow-hidden xl:px-8">
-                            <img
-                                src="https://tailwindui.com/img/ecommerce-images/home-page-02-sale-full-width.jpg"
-                                alt=""
-                                className="h-full w-full object-cover object-center"
-                            />
-                        </div>
-                        <div className="absolute inset-0 bg-white bg-opacity-75" />
-                        <div className="absolute inset-0 bg-gradient-to-t from-white via-white" />
-                    </div>
+                <Testimonials />
 
-                    {/* Sale */}
-                    <section
-                        aria-labelledby="sale-heading"
-                        className="relative mx-auto flex max-w-7xl flex-col items-center px-4 pt-32 text-center sm:px-6 lg:px-8"
-                    >
-                        <div className="mx-auto max-w-2xl lg:max-w-none">
-                            <h2 id="sale-heading" className="text-4xl tracking-tight lg:text-6xl text-gray-900">
-                                Get 25% off during Pride Month
-                            </h2>
-                            <p className="mx-auto mt-4 max-w-xl text-gray-600">
-                                Most of our products are limited releases that won't come back. Get your favorite items while they're in
-                                stock.
-                            </p>
-                            <Link
-                                href="/shop"
-                                className="mt-6 inline-block w-full border border-transparent bg-gray-900 px-8 py-3 font-medium text-white hover:bg-gray-800 sm:w-auto"
-                            >
-                                Get access to our one-time sale
-                            </Link>
-                        </div>
-                    </section>
-
-                    {/* Testimonials */}
-                    <section
-                        aria-labelledby="testimonial-heading"
-                        className="relative mx-auto max-w-7xl px-4 py-24 sm:px-6 lg:px-8 lg:py-32"
-                    >
-                        <div className="mx-auto max-w-2xl lg:max-w-none">
-                            <h2 id="testimonial-heading" className="text-4xl tracking-tight text-gray-900">
-                                What are people saying?
-                            </h2>
-
-                            <div className="mt-16 space-y-16 lg:grid lg:grid-cols-3 lg:gap-x-8 lg:space-y-0">
-                                {testimonials.map((testimonial) => (
-                                    <blockquote key={testimonial.id} className="sm:flex lg:block">
-                                        <svg
-                                            width={20}
-                                            height={14}
-                                            viewBox="0 0 24 18"
-                                            aria-hidden="true"
-                                            className="flex-shrink-0 text-gray-300"
-                                        >
-                                            <path
-                                                d="M0 18h8.7v-5.555c-.024-3.906 1.113-6.841 2.892-9.68L6.452 0C3.188 2.644-.026 7.86 0 12.469V18zm12.408 0h8.7v-5.555C21.083 8.539 22.22 5.604 24 2.765L18.859 0c-3.263 2.644-6.476 7.86-6.451 12.469V18z"
-                                                fill="currentColor"
-                                            />
-                                        </svg>
-                                        <div className="mt-8 sm:ml-6 sm:mt-0 lg:ml-0 lg:mt-10">
-                                            <p className="text-gray-600">{testimonial.quote}</p>
-                                            <cite className="mt-4 block text-sm font-semibold not-italic text-gray-900">
-                                                {testimonial.attribution}
-                                            </cite>
-                                        </div>
-                                    </blockquote>
-                                ))}
-                            </div>
-                        </div>
-                    </section>
-                </div>
             </main>
         </div>
     )

84bff596b4fa9c990ced2b17854a49b7ad562a2d
simozampa <simozampa@ucla.edu>
2023-08-12 17:52:50 -0700

    Revert ISR

--- app/(ecommerce)/shop/[department]/page.tsx
+++ app/(ecommerce)/shop/[department]/page.tsx
@@ -60,15 +60,15 @@ async function getData(department: string | null, category: string | null, subca
     return { products, count, availableBrands, currentDepartment }
 };
 
-export async function generateStaticParams() {
-    const departmentSlugs = ['women', 'men', 'unisex', 'lifestyle']
+// export async function generateStaticParams() {
+//     const departmentSlugs = ['women', 'men', 'unisex', 'lifestyle']
 
-    return departmentSlugs.map((departmentSlug: string) => {
-        return {
-            departmentSlug,
-        }
-    })
-}   
+//     return departmentSlugs.map((department: string) => {
+//         return {
+//             department,
+//         }
+//     })
+// }   
 
 export default async function Shop(
     {

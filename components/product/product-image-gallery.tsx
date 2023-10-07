3a23f845529009b7744dd46372a808200148ab72
pperuzzo <pierlorenzo.peruzzo@gmail.com>
2023-10-06 17:02:39 -0700

    Fixes

--- components/product/product-image-gallery.tsx
+++ components/product/product-image-gallery.tsx
@@ -19,46 +19,6 @@ export default function ProductImageGallery({ product }: { product: ProductWithR
 
     return (
         <>
-            {/* <Transition appear show={currentImage != null} as={Fragment}>
-                <Dialog as="div" className="relative z-10" onClose={() => setCurrentImage(null)}>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <div className="fixed inset-0 bg-black bg-opacity-25" />
-                    </Transition.Child>
-
-                    <div className="fixed inset-0 overflow-y-auto">
-                        <div className="flex min-h-full items-center justify-center p-4 text-center">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 scale-95"
-                                enterTo="opacity-100 scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 scale-100"
-                                leaveTo="opacity-0 scale-95"
-                            >
-                                <Dialog.Panel className="w-auto transform overflow-hidden p-6 text-left align-middle transition-all">
-                                    <Image
-                                        src={currentImage?.src || ""}
-                                        width={500}
-                                        height={500}
-                                        alt={currentImage?.alt || ""}
-                                        className="mx-auto min-h-full"
-                                    />
-                                </Dialog.Panel>
-                            </Transition.Child>
-                        </div>
-                    </div>
-                </Dialog>
-            </Transition> */}
-
             <div className="mt-8 lg:col-span-7 lg:col-start-1 lg:row-span-3 lg:row-start-1 lg:mt-0">
                 <h2 className="sr-only">Images</h2>
 

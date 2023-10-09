324757ed19278faf7460405c1c56a227a090dd98
pperuzzo <pierlorenzo.peruzzo@gmail.com>
2023-10-09 10:40:15 -0700

    Update loading

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,6 +11,7 @@ import InputLabel from "@/components/form/input-label";
 import InputText from "@/components/form/input-text";
 import InputError from "@/components/form/input-error";
 import PrimaryButton from "@/components/form/primary-button";
+import { LoadingSpinner } from "@/components/loading-spinner";
 
 export type LoginFormSchemaType = {
     email: string
@@ -130,13 +131,8 @@ export default function Page() {
                             className={cn("w-full", submitError ? 'mt-2' : '')}
                             disabled={form.processing}
                         >
-                            {form.processing &&
-                                <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                </svg>
-                            }
-                            Login
+                            <span>Login</span>
+                            {form.processing && <LoadingSpinner className="ml-3" />}
                         </PrimaryButton>
                     </div>
                 </div>
